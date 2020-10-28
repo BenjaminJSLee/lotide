6988bb41f58de5a186862f1b56b8b3e0a3a37f50
Benjamin Lee <benjaminjslee@gmail.com>
2020-10-28 19:51:37 +0000

    removed .slice() from middle function and replaced with direct array accesses (more efficient)

--- middle.js
+++ middle.js
@@ -18,7 +18,7 @@ const middle = (arr) => {
   if (arr.length <= 2) return [];
   let mid, midVal;
   mid = Math.floor(arr.length / 2);
-  midVal = arr.length % 2 === 1 ? [arr[mid]] : arr.slice(mid - 1,mid + 1);
+  midVal = arr.length % 2 === 1 ? [arr[mid]] : [arr[mid - 1],arr[mid]];
   return midVal;
 };
 
