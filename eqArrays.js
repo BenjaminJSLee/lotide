b2cf9aa01d03cf9b3cdb709345cb8e083257da94
Benjamin Lee <benjaminjslee@gmail.com>
2020-10-30 22:22:59 +0000

    added recursive properties to function eqArrays so that nested arrays can be compared accurately

--- eqArrays.js
+++ eqArrays.js
@@ -9,7 +9,9 @@ const assertEqual = function(actual, expected) {
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) return false;
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) return false;
+    } else if (arr1[i] !== arr2[i]) return false;
   }
   return true;
 };
@@ -23,3 +25,4 @@ assertEqual(eqArrays([1,2,3],[1,2]),false);
 assertEqual(eqArrays([2,3],[1,2,3]),false);
 assertEqual(eqArrays(['1','2','3'],[1,2,3]),false);
 assertEqual(eqArrays(['1','2','3'],['1','2','3']),true);
+assertEqual(eqArrays([1,[2,[3,[4,[5,6]]]],7],[1,[2,[3,[4,[5,6]]]],7]),true);
