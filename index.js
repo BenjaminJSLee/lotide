8e46e880ae0e58314d68e57e447b809a7ec8aca1
Benjamin Lee <benjaminjslee@gmail.com>
2020-11-02 21:38:26 +0000

    added an index file exporting the currently implemented modules in the lotide repository

---
+++ index.js
@@ -0,0 +1,15 @@
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+
+module.exports = {
+  assertArraysEqual: assertArraysEqual,
+  assertEqual: assertEqual,
+  eqArrays: eqArrays,
+  head:   head,
+  tail:   tail,
+  middle: middle
+};
